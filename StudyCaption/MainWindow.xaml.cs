698a82e7a3343b079468dc06dfb16b4062c837dd
Unknown <qjfrntop12@naver.com>
2017-12-04 21:44:43 +0900

    smi 기본 파싱 기능 추가

--- StudyCaption/MainWindow.xaml.cs
+++ StudyCaption/MainWindow.xaml.cs
@@ -14,35 +14,81 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 using System.ComponentModel;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.IO;
 
 namespace StudyCaption
 {
     public partial class MainWindow : Window
     {
-        List<Caption> CaptionQueue = new List<Caption>();
+        List<Caption> captionQueue = new List<Caption>();
         int listIndex = 0;
-        private BackgroundWorker myThread;
+        DispatcherTimer updateTimer = new DispatcherTimer();
 
         public MainWindow()
         {
+            string[] smiTemp = File.ReadAllLines(@"D:\Anime\코노하나+기담+01.smi");
+            foreach(string line in smiTemp)
+            {
+                if (!line.StartsWith("<SYNC Start"))
+                    continue;
+
+                bool isTagRead = false;
+                string tag = string.Empty;
+                int sync = 0;
+                string caption = string.Empty;
+                StringBuilder stringBuilder;
+                foreach(char c in line)
+                {
+                    switch(c)
+                    {
+                        case '<':
+                            isTagRead = true;
+                            break;
+                        case '>':
+                            isTagRead = false;
+                            if(tag.StartsWith("SYNC Start="))
+                            {
+                                sync = int.Parse(tag.Remove(0, 11));
+                            }
+                            else if(tag == "br")
+                            {
+                                stringBuilder = new StringBuilder(caption);
+                                stringBuilder.Append('\n');
+                                caption = stringBuilder.ToString();
+                            }
+
+                            tag = string.Empty;
+                            break;
+                        default:
+                            if(isTagRead)
+                            {
+                                stringBuilder = new StringBuilder(tag);
+                                stringBuilder.Append(c);
+                                tag = stringBuilder.ToString();
+                            }
+                            else
+                            {
+                                stringBuilder = new StringBuilder(caption);
+                                stringBuilder.Append(c);
+                                caption = stringBuilder.ToString();
+                            }
+                            break;
+                    }
+                }
+                caption = caption.Replace("&nbsp;", "");
+                captionQueue.Add(new Caption(caption, sync));
+            }
+
+            updateTimer.Interval = TimeSpan.FromMilliseconds(0.2);
+            updateTimer.Tick += Update;
+            updateTimer.Start();
+
             InitializeComponent();
 
             Player.Source = new System.Uri(@"D:\Anime\코노하나+기담+1화.mp4");
             Player.LoadedBehavior = MediaState.Manual;
-
-            CaptionQueue.Add(new Caption("사람이 잔뜩 있어~", 7055));
-            CaptionQueue.Add(new Caption("", 8917));
-            CaptionQueue.Add(new Caption("유즈", 9569));
-            CaptionQueue.Add(new Caption("", 10421));
-            CaptionQueue.Add(new Caption("유즈!", 10421));
-            CaptionQueue.Add(new Caption("사람이 잔뜩 있어~", 12440));
-            CaptionQueue.Add(new Caption("멍하니 있다가는 떨어지고 말 거에요?", 13420));
-            CaptionQueue.Add(new Caption("네! 비구니님!", 15008));
-            
-            
-            Thread updateThread = new Thread(Update);
-            updateThread.Start();
-            
         }
 
         private void btn_Play_Click(object sender, RoutedEventArgs e)
@@ -55,15 +101,12 @@ namespace StudyCaption
             Player.Pause();
         }
 
-        private void Update()
+        private void Update(object sender, EventArgs e)
         {
-            while (true)
+            Debug.WriteLine(Player.Position.TotalMilliseconds);
+            if (captionQueue.Count > listIndex && Player.Position.TotalMilliseconds >= captionQueue[listIndex].sync)
             {
-                System.Diagnostics.Debug.WriteLine(CaptionQueue[listIndex].sync + " / " + Player.Position.TotalMilliseconds);
-                if (CaptionQueue.Count > listIndex && Player.Position.TotalMilliseconds >= CaptionQueue[listIndex].sync)
-                {
-                    CaptionText.Text = CaptionQueue[listIndex++].caption;
-                }
+                CaptionText.Text = captionQueue[listIndex++].caption;
             }
         }
 
